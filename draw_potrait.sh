c943695ffbfb5a48d19e0d894d2c2287ccd6f71c
Karthik <xraydiffraction@gmail.com>
2016-09-15 23:11:41 -0700

    Updated draw_potrait

--- draw_potrait.sh
+++ draw_potrait.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-./dna_potrait $1 > coords/$2.txt
-gnuplot -e "image_name='$2'" -e "image_filename='potraits/$2.png'" -e "coord_filename='coords/$2.txt'" draw_image
+echo BEGIN
+date
+
+name=$(basename $1)
+name=${name/.fasta/}
+
+echo $name 
+./dna_potrait $1 > coords/$name.txt
+gnuplot -e "image_name='$name'" -e "image_filename='potraits/$name.png'" -e "coord_filename='coords/$name.txt'" draw_image
+
+echo END
+date
